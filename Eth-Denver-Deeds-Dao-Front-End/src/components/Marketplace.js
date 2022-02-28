5ca49c5c7d1056cfaff894b835788190e82be772
Pascal Weber <p.weber.92@gmail.com>
2022-02-28 23:24:06 +0100

    add slider

--- Eth-Denver-Deeds-Dao-Front-End/src/components/Marketplace.js
+++ Eth-Denver-Deeds-Dao-Front-End/src/components/Marketplace.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import { breakpoint, device } from '../constants/breakpoints';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const Team = () => {
   return (
@@ -8,7 +9,50 @@ const Team = () => {
       <SectionA>
         <ContentWrapper>
           <h2>Marketplace</h2>
-          <p>Coming soon...</p>
+          <Swiper
+          style={{ width: 600 }}
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={"auto"}
+            coverflowEffect={{
+              rotate: 50,
+              stretch: 0,
+              depth: 100,
+              modifier: 1,
+              slideShadows: false,
+            }}
+            pagination={true}
+            className="mySwiper"
+          >
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-1.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-2.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-3.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-4.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-5.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-6.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-7.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-8.jpg" alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img src="https://swiperjs.com/demos/images/nature-9.jpg" alt="" />
+            </SwiperSlide>
+          </Swiper>
         </ContentWrapper>
       </SectionA>
     </>
--- Eth-Denver-Deeds-Dao-Front-End/src/index.js
+++ Eth-Denver-Deeds-Dao-Front-End/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.css';
 import './index.css';
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
